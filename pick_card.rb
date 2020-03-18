394fb2808cf42474b78c7c30eff336877d03a31d
neo712106 <egubka712248@gmail.com>
2020-03-18 17:16:46 +0200

    Version 2 of the program

--- pick_card.rb
+++ pick_card.rb
@@ -1,2 +1,12 @@
 # Encode UTF-8
-puts "Cart game here..."
+puts "Random showing of game card"
+
+sign = ["♦", "♠", "♥", "♣"]
+
+number = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+
+puts "#{number.sample} #{sign.sample}"
+
+
+
+
